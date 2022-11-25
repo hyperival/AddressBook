d5c36eac37f52da4832f39239e0764bcebbc3108
Hyperival <sanjaygautam779@gmail.com>
2022-11-25 20:08:30 +0530

    Ability to edit existing Contacts to Address book

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -8,15 +8,17 @@ namespace AddressBook
 {
     internal class Contacts
     {
-      
-            public string First_Name { get; set; }
-            public string Last_Name { get; set; }
-            public string Present_Address { get; set; }
-            public string City { get; set; }
-            public string State { get; set; }
-            public string Email_Id { get; set; }
-            public string Phone_Number { get; set; }
-            public string Zip { get; set; }
+        public string first_Name { get; set; }
+        public string last_Name { get; set; }
+        public string present_Address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string email_Id { get; set; }
+        public string phone_Number { get; set; }
+        public string zip { get; set; }
+        public override string ToString()
+        {
+            return "FirstName: " + this.first_Name + " \nLastName: " + this.last_Name + " \nPresent Address: " + this.present_Address + " \nCity: " + this.city + " \nState: " + this.state + " \nZipcode: " + this.zip + " \nPhoneNumber: " + this.phone_Number + " \nEmail: " + this.email_Id;
         }
     }
-
+}
