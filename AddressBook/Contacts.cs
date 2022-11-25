a8a2431f70e4aa898266236f2e6a04a70b6b13f4
Hyperival <sanjaygautam779@gmail.com>
2022-11-25 19:38:00 +0530

    Ability to create contats in addressbook

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contacts
+    {
+      
+            public string First_Name { get; set; }
+            public string Last_Name { get; set; }
+            public string Present_Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Email_Id { get; set; }
+            public string Phone_Number { get; set; }
+            public string Zip { get; set; }
+        }
+    }
+
