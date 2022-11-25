8afa087e33e221c8b5ec8e2f14ceb62b8c07f3d1
Hyperival <sanjaygautam779@gmail.com>
2022-11-25 19:52:13 +0530

    Ability to add new Contacts to Address book using oops

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -11,26 +11,9 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            List<Contacts> Addressbook = new List<Contacts>();
-            Contacts contact = new Contacts();
-            Console.WriteLine("Add a New Contact:");
-            Console.WriteLine("Enter your first_name");
-            contact.First_Name = Console.ReadLine();
-            Console.WriteLine("Enter your last_name");
-            contact.Last_Name = Console.ReadLine();
-            Console.WriteLine("Enter your Present_address");
-            contact.Present_Address = Console.ReadLine();
-            Console.WriteLine("Enter your City");
-            contact.City = Console.ReadLine();
-            Console.WriteLine("Enter your State");
-            contact.State = Console.ReadLine();
-            Console.WriteLine("Enter your Email_Id");
-            contact.Email_Id = Console.ReadLine();
-            Console.WriteLine("Enter your Phone_Number");
-            contact.Phone_Number = Console.ReadLine();
-            Console.WriteLine("Enter your ZipCode");
-            contact.Zip = Console.ReadLine();
-            Addressbook.Add(contact);
+
+            Address address = new Address();
+            address.Entry();
 
 
 
